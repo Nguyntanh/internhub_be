3ac081355067a01c50f22485181452dd19b2431a
Nguyntanh <nguyntanh2k5@gmail.com>
2026-01-13 18:20:01 +0700

    first commit

---
+++ src/main/java/com/example/internhub_be/InternhubBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.internhub_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternhubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternhubBeApplication.class, args);
+	}
+
+}
