042616ade5a13cbf932f86fc5a75ddba2b8c3dca
minhha205 <148325119+haminh05@users.noreply.github.com>
2026-03-02 23:04:48 +0700

    feat: implement skill task management module

--- src/main/java/com/example/internhub_be/repository/SkillRepository.java
+++ src/main/java/com/example/internhub_be/repository/SkillRepository.java
@@ -3,5 +3,9 @@ package com.example.internhub_be.repository;
 import com.example.internhub_be.domain.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SkillRepository extends JpaRepository<Skill, Integer> {
+import java.util.List;
+
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    List<Skill> findByParentIdIsNull();
+    List<Skill> findByParentId(Long parentId);
 }
